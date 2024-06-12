5f0ea687e01c9f62db764d3906c11e32891ded2b
Anthony Elison <tonyelison37@gmail.com>
2024-06-12 16:32:21 -0600

    Add testing routes, add global stylesheet

--- pages/about.vue
+++ pages/about.vue
@@ -3,9 +3,8 @@ const route = useRoute();
 </script>
 
 <template>
-  <NuxtLink to="/about">About</NuxtLink>
   <div>
-    <h1>Nuxt Routing set up successfully!</h1>
+    <h1>About Us</h1>
     <p>Current route: {{ route.path }}</p>
   </div>
 </template>
